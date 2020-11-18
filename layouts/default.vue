1328b59a60af498dae89b24809bcacedb8ab0405
dkasabovn <dkasabovn@gmail.com>
2020-11-17 21:16:10 -0600

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,22 @@
   </div>
 </template>
 
+<script>
+export default {
+  head() {
+    return {
+      title: 'HypeATX',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'A company dedicated to the culture.'
+        }
+      ]
+    }
+  }
+}
+</script>
+
 <style>
 </style>
